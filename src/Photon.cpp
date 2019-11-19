b137b6d477ac6ddbe7177fab3c91a32549c8d2eb
ryoyumo <yumo.after.fun@gmail.com>
2019-11-20 00:06:00 +0900

    Add Photon Scene

--- src/Photon.cpp
+++ src/Photon.cpp
@@ -6,3 +6,34 @@
 //
 
 #include "Photon.hpp"
+#include "ofMain.h"
+#include "ofxPubSubOsc.h"
+
+namespace orf2019 {
+void Photon::setup(){
+    // OSC
+    ofxSubscribeOsc(54321, "/img", index_);
+    
+    ofJson js;
+    js = ofLoadJson("photon/photon-1.json");
+    for(auto i = 0; i < js.size(); ++i){
+        auto d = js[ofToString(i)];
+        for(auto dd : d){
+            Data data;
+            data.x = dd["x"];
+            data.y = dd["y"];
+            data.radius = dd["radius"];
+            img_data_map_[i].push_back(data);
+        }
+    }
+}
+    
+void Photon::drawLaser(){
+    auto data = img_data_map_[index_];
+    for(auto d : data){
+        laser_->drawEllipse(d.x, d.y, d.radius, d.radius);
+    }
+}
+    
+    
+}
--- src/Photon.hpp
+++ src/Photon.hpp
@@ -8,6 +8,27 @@
 #ifndef Photon_hpp
 #define Photon_hpp
 
-#include <stdio.h>
+#include "LaserOSC.hpp"
+#include "SceneManager.hpp"
+#include "glm/glm.hpp"
+#include <unordered_map>
+
+namespace orf2019 {
+class Photon : public BaseScene{
+public:
+    Photon():index_{0}{}
+    void setLaser(LaserOSC* laser) override { laser_ = laser; }
+    void setup() override;
+    void drawLaser() override;
+    void drawVisual() override {}
+    
+private:
+    struct Data { float x, y, radius; };
+    std::map<int, std::vector<Data>> img_data_map_;
+    LaserOSC* laser_;
+    std::size_t index_;
+
+};
+}
 
 #endif /* Photon_hpp */
